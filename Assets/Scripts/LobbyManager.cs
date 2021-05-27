a2784e5388cec73e6fa85f51965e0c3efa9a1d0a
as <>
2021-05-27 22:17:11 +0900

    ログインネームの取得

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [Header("Login UI")]
+    public InputField playerNameInputField;
+
+
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+
+
+    #region UI Callback Methods
+    public void OnEnterGameButtonClicked()
+    {
+        string playerName = playerNameInputField.text;
+
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        else
+        {
+            Debug.Log("Player name is invalid or enpty!");
+        }
+    }
+    #endregion
+
+
+    #region PHOTON Callback Method
+    public override void OnConnected()
+    {
+        Debug.Log("We connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon Server");
+    }
+    #endregion
+
+}
