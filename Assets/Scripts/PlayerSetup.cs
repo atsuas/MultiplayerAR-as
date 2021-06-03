b1b97e2f9a9149b46899d700e536690b801c9c7f
as <>
2021-06-03 21:38:44 +0900

    選択したプレイヤーオブジェクトのみを動かす

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPun
+{
+    
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            //プレイヤーはローカルプレイヤー
+            transform.GetComponent<MomentController>().enabled = true;
+            transform.GetComponent<MomentController>().joystick.gameObject.SetActive(true);
+        }
+        else
+        {
+            //プレイヤーはリモートプレイヤー
+            transform.GetComponent<MomentController>().enabled = false;
+            transform.GetComponent<MomentController>().joystick.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
