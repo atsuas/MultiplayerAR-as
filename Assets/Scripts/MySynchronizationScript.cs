4a8c1dcbbae7228d5e6e6dd90de11beb4e704682
as <>
2021-06-04 14:58:27 +0900

    ２つのゲーム画面をシンクロさせる

---
+++ Assets/Scripts/MySynchronizationScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MySynchronizationScript : MonoBehaviour, IPunObservable
+{
+    Rigidbody rb;
+    PhotonView photonView;
+
+    Vector3 networkedPosition;
+    Quaternion networkedRotation;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        photonView = GetComponent<PhotonView>();
+
+        networkedPosition = new Vector3();
+        networkedRotation = new Quaternion();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+        {
+            rb.position = Vector3.MoveTowards(rb.position, networkedPosition, Time.fixedDeltaTime);
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, networkedRotation, Time.fixedDeltaTime * 100);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //Then, photonView is mine and I am the one who controls this player.
+            //should send position, velocity etc. data to the other players
+            stream.SendNext(rb.position);
+            stream.SendNext(rb.rotation);
+        }
+        else
+        {
+            //Called on my player gameobject that exists in remote player's game
+            networkedPosition = (Vector3)stream.ReceiveNext();
+            networkedRotation = (Quaternion)stream.ReceiveNext();
+
+        }
+    }
+}
